bc82259a7a109b5e8cacef9715004e3adefec08e
Ryan Marks <ryan.marks@pantheon.io>
2022-06-05 19:01:37 +0100

    List /usr/local/bin/terminus

--- prepare.php
+++ prepare.php
@@ -34,6 +34,7 @@ if ( ! empty( $WPT_SSH_PRIVATE_KEY_BASE64 ) ) {
 		'chmod +x terminus',
 		'pwd',
 		'ls -al',
+		'ls -al /usr/local/bin/terminus'
 		'terminus auth:login --machine-token=' . $TERMINUS_MACHINE_TOKEN,
 		'terminus wp ' . $PANTHEON_SITE_NAME . '.' . $PANTHEON_SITE_ENV . ' -- cli info',
 	) );
